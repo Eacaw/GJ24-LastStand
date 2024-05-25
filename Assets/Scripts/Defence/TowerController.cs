cce18018bbb0f252cb3cf80997373fb0e2c65232
Michael Montagnese <mmont429@gmail.com>
2024-05-25 22:10:51 +0100

    Add tower controller script

---
+++ Assets/Scripts/Defence/TowerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    public int health = 2;  // Initial health of the target
+    public int damage = 1;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage();  // Decrease health by 1 when collided with an enemy
+        }
+    }
+
+    void TakeDamage()
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);  // Remove the object when health reaches 0
+        }
+    }
+}
