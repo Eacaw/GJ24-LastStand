2b27b48178ed61d329847e26fc40d33542b3ee34
Eacaw <davidpinchen@gmail.com>
2024-05-26 11:04:41 +0100

    Rotate camera in 45deg increments

--- Assets/Scripts/PlayerInput/CameraController.cs
+++ Assets/Scripts/PlayerInput/CameraController.cs
@@ -10,14 +10,26 @@ public class CameraController : MonoBehaviour
     private Vector3 rot0Pos = new Vector3(0, 13.5f, -12);
     private Vector3 rot0Rot = new Vector3(55, 0, 0);
 
-    private Vector3 rot1Pos = new Vector3(-12, 13.5f, 0);
-    private Vector3 rot1Rot = new Vector3(55, 90, 0);
+    private Vector3 rot1Pos = new Vector3(-8.5f, 13.5f, -8.5f);
+    private Vector3 rot1Rot = new Vector3(55, 45, 0);
 
-    private Vector3 rot2Pos = new Vector3(0, 13.5f, 12);
-    private Vector3 rot2Rot = new Vector3(55, 180, 0);
+    private Vector3 rot2Pos = new Vector3(-12, 13.5f, 0);
+    private Vector3 rot2Rot = new Vector3(55, 90, 0);
 
-    private Vector3 rot3Pos = new Vector3(12, 13.5f, 0);
-    private Vector3 rot3Rot = new Vector3(55, 270, 0);
+    private Vector3 rot3Pos = new Vector3(-8.5f, 13.5f, 8.5f);
+    private Vector3 rot3Rot = new Vector3(55, 135, 0);
+
+    private Vector3 rot4Pos = new Vector3(0, 13.5f, 12);
+    private Vector3 rot4Rot = new Vector3(55, 180, 0);
+
+    private Vector3 rot5Pos = new Vector3(8.5f, 13.5f, 8.5f);
+    private Vector3 rot5Rot = new Vector3(55, 225, 0);
+
+    private Vector3 rot6Pos = new Vector3(12, 13.5f, 0);
+    private Vector3 rot6Rot = new Vector3(55, 270, 0);
+
+    private Vector3 rot7Pos = new Vector3(8.5f, 13.5f, -8.5f);
+    private Vector3 rot7Rot = new Vector3(55, 315, 0);
 
     private void Start() { }
 
@@ -26,7 +38,7 @@ public class CameraController : MonoBehaviour
         rotation--;
         if (rotation < 0)
         {
-            rotation = 3;
+            rotation = 7;
         }
         RotateCamera();
     }
@@ -34,7 +46,7 @@ public class CameraController : MonoBehaviour
     public void RotateCameraLeft()
     {
         rotation++;
-        if (rotation > 3)
+        if (rotation > 7)
         {
             rotation = 0;
         }
@@ -60,6 +72,22 @@ public class CameraController : MonoBehaviour
             case 3:
                 transform.position = rot3Pos;
                 transform.rotation = Quaternion.Euler(rot3Rot);
+                break;
+            case 4:
+                transform.position = rot4Pos;
+                transform.rotation = Quaternion.Euler(rot4Rot);
+                break;
+            case 5:
+                transform.position = rot5Pos;
+                transform.rotation = Quaternion.Euler(rot5Rot);
+                break;
+            case 6:
+                transform.position = rot6Pos;
+                transform.rotation = Quaternion.Euler(rot6Rot);
+                break;
+            case 7:
+                transform.position = rot7Pos;
+                transform.rotation = Quaternion.Euler(rot7Rot);
                 break;
         }
     }
