1ece411b235fce02f463b1aebc9fad3922e9c873
Eacaw <davidpinchen@gmail.com>
2024-05-26 11:01:30 +0100

    Implement Camera rotations

--- Assets/Scripts/PlayerInput/CameraController.cs
+++ Assets/Scripts/PlayerInput/CameraController.cs
@@ -5,73 +5,62 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    private float panSpeed = 5f,
-        scrollSpeed = 5,
-        minY = 1f,
-        maxY = 20f,
-        rotationSpeed = 5f;
+    private int rotation = 0;
 
-    [SerializeField]
-    private Vector2 panLimit = new Vector2(11.5f, 7);
+    private Vector3 rot0Pos = new Vector3(0, 13.5f, -12);
+    private Vector3 rot0Rot = new Vector3(55, 0, 0);
 
-    private float horizontalRotation = -50f,
-        verticalRotation = 50f;
+    private Vector3 rot1Pos = new Vector3(-12, 13.5f, 0);
+    private Vector3 rot1Rot = new Vector3(55, 90, 0);
 
-    private float speedModifier = 3f;
+    private Vector3 rot2Pos = new Vector3(0, 13.5f, 12);
+    private Vector3 rot2Rot = new Vector3(55, 180, 0);
 
-    private void Start()
+    private Vector3 rot3Pos = new Vector3(12, 13.5f, 0);
+    private Vector3 rot3Rot = new Vector3(55, 270, 0);
+
+    private void Start() { }
+
+    public void RotateCameraRight()
     {
-        // Set up the initial position and rotation of the camera
-        transform.position = new Vector3(5, 7.5f, -5);
-        transform.rotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
+        rotation--;
+        if (rotation < 0)
+        {
+            rotation = 3;
+        }
+        RotateCamera();
     }
 
-    private void Update()
+    public void RotateCameraLeft()
     {
-        Vector3 pos = transform.position;
-        Quaternion rot = transform.rotation;
-
-        if (Input.GetKey("w"))
+        rotation++;
+        if (rotation > 3)
         {
-            pos += transform.forward * panSpeed * Time.deltaTime;
+            rotation = 0;
         }
-        if (Input.GetKey("s"))
-        {
-            pos -= transform.forward * panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("d"))
-        {
-            pos += transform.right * panSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("a"))
-        {
-            pos -= transform.right * panSpeed * Time.deltaTime;
-        }
+        RotateCamera();
+    }
 
-        float scroll = Input.GetAxis("Mouse ScrollWheel");
-        pos += transform.forward * scroll * scrollSpeed * 100f * Time.deltaTime;
-
-        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
-        pos.y = Mathf.Clamp(pos.y, minY, maxY);
-        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
-
-        transform.position = pos;
-
-        if (Input.GetMouseButton(1))
+    private void RotateCamera()
+    {
+        switch (rotation)
         {
-            horizontalRotation += Input.GetAxis("Mouse X") * rotationSpeed;
-            verticalRotation -= Input.GetAxis("Mouse Y") * rotationSpeed;
-            verticalRotation = Mathf.Clamp(verticalRotation, -80f, 80f);
-            transform.rotation = Quaternion.Euler(verticalRotation, horizontalRotation, 0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            panSpeed *= speedModifier;
-        }
-        if (Input.GetKeyUp(KeyCode.LeftShift))
-        {
-            panSpeed /= speedModifier;
+            case 0:
+                transform.position = rot0Pos;
+                transform.rotation = Quaternion.Euler(rot0Rot);
+                break;
+            case 1:
+                transform.position = rot1Pos;
+                transform.rotation = Quaternion.Euler(rot1Rot);
+                break;
+            case 2:
+                transform.position = rot2Pos;
+                transform.rotation = Quaternion.Euler(rot2Rot);
+                break;
+            case 3:
+                transform.position = rot3Pos;
+                transform.rotation = Quaternion.Euler(rot3Rot);
+                break;
         }
     }
 }
