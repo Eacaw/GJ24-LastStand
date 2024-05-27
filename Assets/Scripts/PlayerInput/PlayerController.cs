3d09151a35331a0370558d464ef1d9f866d9f670
Michael Montagnese <mmont429@gmail.com>
2024-05-27 17:37:19 +0100

    Update player controller to update UI

--- Assets/Scripts/PlayerInput/PlayerController.cs
+++ Assets/Scripts/PlayerInput/PlayerController.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     public int currency = 20;  // Initial currency balance
+    public UIDocument UIDocument;
+
+
+    private VisualElement root;
+    private Label currencyCounter;
+
+    void Start()
+    {
+        root = UIDocument.rootVisualElement;
+        currencyCounter = root.Q<Label>("CurrencyCount");
+        currencyCounter.text = currency.ToString();
+    }
 
     public void AddCurrency(int amount)
     {
         currency += amount;
         Debug.Log("Currency added: " + amount + ". New balance: " + currency);
+        currencyCounter.text = currency.ToString();
     }
 
     public bool SubtractCurrency(int amount)
@@ -16,6 +30,7 @@ public class PlayerController : MonoBehaviour
         {
             currency -= amount;
             Debug.Log("Currency subtracted: " + amount + ". New balance: " + currency);
+            currencyCounter.text = currency.ToString();
             return true;
         }
         else
